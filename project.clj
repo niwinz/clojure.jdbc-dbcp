cf4293f48bc16aeab74d9219aee07202309fc579
Andrey Antukh <niwi@niwi.be>
2014-08-27 10:47:42 +0200

    Import from clojure.jdbc codebase.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure.jdbc/clojure.jdbc-dbcp "0.2.0"
+  :description "Apache Commons JDBC connection pool implementation for clojure.jdbc."
+  :url "http://github.com/niwibe/clojure.jdbc"
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.apache.commons/commons-dbcp2 "2.0.1"]])
