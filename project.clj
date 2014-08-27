8042456d0d7ae779db0af2485b512cf69a1ee8ea
Andrey Antukh <niwi@niwi.be>
2014-08-27 23:09:55 +0200

    Fix project url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure.jdbc/clojure.jdbc-dbcp "0.3.0"
   :description "Apache Commons JDBC connection pool implementation for clojure.jdbc."
-  :url "http://github.com/niwibe/clojure.jdbc"
+  :url "http://github.com/niwibe/clojure.jdbc-dbcp"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
