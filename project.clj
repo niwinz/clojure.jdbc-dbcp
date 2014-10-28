e3bbba7cd9cac7a097708b0c1c5a0cf6e26a8d71
Andrey Antukh <niwi@niwi.be>
2014-10-28 10:38:33 +0100

    Update project.clj.

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject clojure.jdbc/clojure.jdbc-dbcp "0.3.0"
+(defproject clojure.jdbc/clojure.jdbc-dbcp "0.3.1"
   :description "Apache Commons JDBC connection pool implementation for clojure.jdbc."
   :url "http://github.com/niwibe/clojure.jdbc-dbcp"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.commons/commons-dbcp2 "2.0.1"]])
+                 [clojure.jdbc "0.3.0"]
+                 [org.apache.commons/commons-dbcp2 "2.0.1"]]
+
+  :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.176"]]}})
+
