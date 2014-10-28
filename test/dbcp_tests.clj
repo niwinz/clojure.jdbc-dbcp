800ec322742427cbfb99ac5cee4b2291705b8b7c
Andrey Antukh <niwi@niwi.be>
2014-10-28 10:38:13 +0100

    Add tests.

---
+++ test/dbcp_tests.clj
@@ -0,0 +1,17 @@
+(ns dbcp-tests
+  (:require [jdbc.core :refer :all]
+            [jdbc.pool.dbcp :as pool]
+            [clojure.test :refer :all]))
+
+(def dbspec {:subprotocol "h2"
+             :subname ":mem:"})
+
+(deftest datasource-spec
+  (testing "Connection pool testing."
+    (let [spec (pool/make-datasource-spec dbspec)]
+      (is (instance? javax.sql.DataSource (:datasource spec)))
+      (with-open [conn (make-connection spec)]
+        (is (instance? jdbc.types.Connection conn))
+        (is (instance? java.sql.Connection (:connection conn)))
+        (let [result (query conn ["SELECT 1 + 1 as foo;"])]
+          (is (= [{:foo 2}] result)))))))
